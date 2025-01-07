50cc22876490270e910e7a29be3004db06a3c4ce
gbmahone <oliveiragabrielam@hotmail.com>
2025-01-07 12:20:07 +0000

    Good solution

--- application/Program.java
+++ application/Program.java
@@ -1,6 +1,7 @@
 package tratamentoDeExcecoes.application;
 
 import tratamentoDeExcecoes.entities.Reservation;
+import tratamentoDeExcecoes.exceptions.DomainException;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -8,23 +9,20 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class Program {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
         // meu metodo main é um metodo pode lancar uma exceção desse tipo (ParseException), ele não tem que tratar esse tipo de exceção
         // temos duas opções tratar a exceção ou propagar a exceção dentro do codigo usando a palavra "throws"
         Scanner sc = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-        System.out.print("Room number: ");
-        int number = sc.nextInt();
-        System.out.print("Check-in date (dd/MM/yyyy): ");
-        Date checkIn = sdf.parse(sc.next());
-        System.out.print("Check-out date (dd/MM/yyyy): ");
-        Date checkOut = sdf.parse(sc.next());
+        try {
+            System.out.print("Room number: ");
+            int number = sc.nextInt();
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            Date checkIn = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            Date checkOut = sdf.parse(sc.next());
 
-        if (!checkOut.after(checkIn)) {
-            System.out.println("Error in reservation: Check-out date must be after check-in date");
-        }
-        else {
             Reservation reservation = new Reservation(number, checkIn, checkOut);
             System.out.println("Reservation: " + reservation);
 
@@ -36,14 +34,19 @@ public class Program {
             checkOut = sdf.parse(sc.next());
 
 
-            String error = reservation.updateDates(checkIn, checkOut);
-            if (error != null){
-                System.out.println("Error in reservation: " + error);
-            }
-            else {
-                System.out.println("Reservation: " + reservation);
-            }
+            reservation.updateDates(checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
         }
-
+        catch (ParseException e) {
+            System.out.println("Invalid date format");
+        }
+        catch (DomainException e) {
+            System.out.println("Error in reservation: " + e.getMessage());
+            //o getMessege vai ser a frase que eu lançei na exceção dentro do metodo updatesDates
+        }
+        catch (RuntimeException e){
+            System.out.println("Unexpected error");
+            //qualquer outra exceção inesperada eu posso dar essa mensagem
+        }
     }
 }
--- entities/Reservation.java
+++ entities/Reservation.java
@@ -1,5 +1,7 @@
 package tratamentoDeExcecoes.entities;
 
+import tratamentoDeExcecoes.exceptions.DomainException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -13,6 +15,9 @@ public class Reservation {
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        if (!checkOut.after(checkIn)) {
+            throw new DomainException("Check-out date must be after check-in date");
+        }
         this.roomNumber = roomNumber;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
@@ -39,19 +44,20 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
     }
 
-    public String updateDates(Date checkIn, Date checkOut) {
+    public void updateDates(Date checkIn, Date checkOut) {
 
         Date now = new Date();
         if (checkIn.before(now) || checkOut.before(now)){
-            return "Reservation dates for update must be future";
+            throw new DomainException("Reservation dates for update must be future");
         }
+        //mudei o public pra void e como vou declarar uma frase em string vai dar erro
+        // mas usando o throw eu posso lançar uma exceção e continuar com a frase mesmo usando o void no public
         if (!checkOut.after(checkIn)){
-            return "Check-out date must be after check-in date";
+            throw new DomainException("Check-out date must be after check-in date");
         }
 
         this.checkIn = checkIn;
         this.checkOut = checkOut;
-        return null;
     }
 
     @Override
